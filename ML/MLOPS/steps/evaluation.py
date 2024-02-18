f8315261381f29c2874c8872323cc36a778b2e28
Akash Santebennur Matada Umapathi <45820053+akashsmu@users.noreply.github.com>
2024-02-18 14:39:14 -0500

    added ML Flow

--- ML/MLOPS/steps/evaluation.py
+++ ML/MLOPS/steps/evaluation.py
@@ -6,8 +6,14 @@ import pandas as pd
 from src.evaluation import MSE,RMSE,R2
 from typing import Tuple
 from typing_extensions import Annotated
+from zenml.client import Client
+import mlflow
+experiment_tracker = Client().active_stack.experiment_tracker
 
-@step
+
+
+
+@step(experiment_tracker=experiment_tracker.name)
 def evaluation_model(model:RegressorMixin,
                      X_test:pd.DataFrame,
                      y_test:pd.DataFrame,
@@ -20,10 +26,13 @@ def evaluation_model(model:RegressorMixin,
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("mse",mse)
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("r2",r2)
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("rmse",rmse)
         return mse,r2,rmse
     except Exception as e:
         pass
