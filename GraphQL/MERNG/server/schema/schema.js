a91ee90d6a18148299f7633f284e9c271bbb1336
Akash Santebennur Matada Umapathi <45820053+akashsmu@users.noreply.github.com>
2024-05-25 20:44:09 -0400

    mongoose

--- GraphQL/MERNG/server/schema/schema.js
+++ GraphQL/MERNG/server/schema/schema.js
@@ -5,9 +5,10 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
-const clientTyp = new GraphQLObjectType({
+const clientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
     id: { type: GraphQLID },
@@ -17,9 +18,44 @@ const clientTyp = new GraphQLObjectType({
   }),
 });
 
+const projectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: clientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientID);
+      },
+    },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(projectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: projectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(clientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: clientType,
       args: { id: { type: GraphQLID } },
